ffa1299b4815cac600bc38b9aac366790b3e12bd
Rocky Gray <rocky.grayjr@gmail.com>
2018-07-31 01:36:03 -0400

    flesh out metrics

--- scripts/bandwidth.js
+++ scripts/bandwidth.js
@@ -1,14 +1,42 @@
+const os = require('os');
 const speedTest = require('speedtest-net');
 const StatsD = require('node-statsd');
 
 const statsHost = process.env.STATSD_HOST;
 const statsPort = process.env.STATSD_PORT;
-const statsPrefix = process.env.STATSD_PREFIX;
+const statsBasePrefix = process.env.STATSD_PREFIX;
 const statsSuffix = process.env.STATSD_SUFFIX;
 
-const client = new StatsD(statsHost, statsPort, statsPrefix, statsSuffix);
+const connectionType = process.argv[2];
 
-console.log('Initializing test...');
+if (connectionType === '-h' || connectionType === '--help') {
+  console.log(`${process.argv[1]} <connectionType> <host>`);
+  process.exit(0);
+}
+
+if (!connectionType) {
+  console.error();
+  console.error('connectionType is required');
+  console.log(`${process.argv[1]} <connectionType> <host>`);
+  process.exit(1);
+}
+
+const validConnectionTypes = ['wifi', 'ethernet'];
+if (!validConnectionTypes.includes(connectionType)) {
+  console.error();
+  console.error(`Invalid connection type: ${connectionType}`);
+  console.error(`The connection type should be one of: ${validConnectionTypes.join(', ')}`);
+}
+
+const host = process.argv[3] || os.hostname();
+
+const prefix = [statsBasePrefix, connectionType, host]
+  .filter(p => !!p)
+  .join('.');
+
+const client = new StatsD(statsHost, statsPort, `${prefix}.`, statsSuffix);
+
+console.log(`${new Date()} - Initializing test... ${prefix}`);
 
 const sendMetric = (metric, data) => {
   client.gauge(metric, data, (err) => {
